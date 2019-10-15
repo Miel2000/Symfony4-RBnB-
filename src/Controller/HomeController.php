9b95e2bda4fbe425688055ed3e11e64826cf15b3
Miel2000 <fabien.orsn@gmail.com>
2019-10-15 17:13:04 +0200

    Skelet v1

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+class HomeController extends AbstractController
+{
+
+
+   /**
+     * @Route("/hello/{prenom}", name="hello_prenom")
+     * @Route("/hello/{prenom}/age/{age}", name="hello_prenom_age")
+     * @Route("/hello/", name="hello_base")
+     * 
+     */
+
+    public function hello($prenom = 'yeves' , $age = 0){
+
+        return $this->render('home/hello.html.twig', [
+            'controller_name' => 'HomeController',
+            'prenom' => $prenom,
+            'age' => $age
+            
+        ]);
+    }
+    
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'HomeController'
+           
+        ]);
+    }
+
+
+}
